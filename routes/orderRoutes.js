c6661121e4fa49e48c9370cbcf8183b8adfd4507
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-03-12 11:27:23 +0600

    debugging

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { getAllOrders, myOrders, getSingleOrder, newOrder, updateOrder, deleteOrder } = require('../controllers/orderController');
 const { isAuthenticatedUser, authorizeRole } = require('../middlewares/auth');
 const router = express.Router();
-router.get("/admin", isAuthenticatedUser, authorizeRole("admin"), getAllOrders)
+router.post("/new-order", isAuthenticatedUser, newOrder)
 router.get("/my-orders", isAuthenticatedUser, myOrders)
 router.get("/:id", getSingleOrder)
-router.post("/new-order", isAuthenticatedUser, newOrder)
-router.put("/update/:id", isAuthenticatedUser, authorizeRole("admin"), updateOrder)
+router.get("/admin", isAuthenticatedUser, authorizeRole("admin"), getAllOrders)
 router.delete("/delete/:id", deleteOrder)
+router.put("/update/:id", isAuthenticatedUser, authorizeRole("admin"), updateOrder)
+
 
 module.exports = router;
