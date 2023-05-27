a31f9cbdaf4eb198de240e0f6ae8e24b98060eab
shahriarlimon <96817971+shahriarlimon@users.noreply.github.com>
2023-05-27 11:27:49 +0600

    changed cookies option

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,9 +3,7 @@ exports.sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
     /* options for cookie */
     const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
+        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true
     }
     res.status(statusCode).cookie("token", token, options).json({
